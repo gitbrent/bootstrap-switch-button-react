153ce4f7388b26c6afb629405ed0c86ed9935a4c
GitBrent <gitbrent@users.noreply.github.com>
2019-03-27 22:38:38 -0500

    update for beta.3

---
+++ lib/bootstrap-switch-button.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for pptxgenjs 2.3.0
+// Project: https://gitbrent.github.io/bootstrap-switch-button-react/
+// Definitions by: Brent Ely <https://github.com/gitbrent/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+export as namespace BootstrapSwitchButton;
+
+export = BootstrapSwitchButton;
+
+declare class BootstrapSwitchButton {
+// TODO:
+}
